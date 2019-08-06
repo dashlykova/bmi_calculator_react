728ca696294934acdf976244a415da7e54b4814e
Daria Lykova <daria.lykova@gmail.com>
2019-08-06 10:54:18 +0200

    Creates an imperial method option for the calculator/Tries to implement Semantic UI unsuccessfully, back to css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import DisplayResult from './Components/displayResult';
 
 
+
 class App extends Component {
   constructor(props) {
     super(props)
@@ -16,17 +17,28 @@ class App extends Component {
     return (
       <div>
         <div>
-          <label>Weight (kg)</label>
-          <input name="weight" value={this.state.weight} onChange={(e) => this.setState({ weight: e.target.value })} />
+          <label>Weight</label>
+          <input
+            placeholder={this.state.method === 'metric' ? 'kg' : 'lbs'}
+            name="weight"
+            value={this.state.weight}
+            onChange={(e) => this.setState({ weight: e.target.value })}
+          />
         </div>
         <div>
-          <label>Height (cm)</label>
-          <input name="height" value={this.state.height} onChange={(e) => this.setState({ height: e.target.value })} />
+          <label>Height</label>
+          <input
+            placeholder={this.state.method === 'metric' ? 'cm' : 'in'}
+            name="height"
+            value={this.state.height}
+            onChange={(e) => this.setState({ height: e.target.value })}
+          />
         </div>
 
         <DisplayResult
           weight={this.state.weight}
           height={this.state.height}
+          method={this.state.method}
         />
       </div>
     );
